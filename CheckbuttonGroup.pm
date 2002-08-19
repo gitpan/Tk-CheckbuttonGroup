3ba6320992a73417a9dcc964389ce355e7fe903f
Joseph Annino <jannino@jannino.com>
2002-08-19 22:03:34 +0000

    Import of JANNINO/Tk-CheckbuttonGroup-0.2.1 from CPAN.
    gitpan-cpan-distribution: Tk-CheckbuttonGroup
    gitpan-cpan-version:      0.2.1
    gitpan-cpan-path:         JANNINO/Tk-CheckbuttonGroup-0.2.1.tgz
    gitpan-cpan-author:       JANNINO
    gitpan-cpan-maturity:     released

--- CheckbuttonGroup.pm
+++ CheckbuttonGroup.pm
@@ -20,7 +20,7 @@
 #    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #
 
-$Tk::CheckbuttonGroup::VERSION = '0.2';
+$Tk::CheckbuttonGroup::VERSION = '0.2.1';
 package Tk::CheckbuttonGroup;
 
 use Tk::widgets qw( Frame Checkbutton );
--- test.pl
+++ test.pl
@@ -8,7 +8,7 @@
 
 BEGIN { $| = 1; print "1..6\n"; }
 END {print "not ok 1\n" unless $loaded;}
-use TK::CheckbuttonGroup;
+use Tk::CheckbuttonGroup;
 $loaded = 1;
 print "ok 1\n";
 
